83f6f2ffa0d7acc597899aced40f0fd94147692e
jbatistao <jossey.batista@gmail.com>
2026-05-11 23:04:31 -0500

    Fix deploy

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
